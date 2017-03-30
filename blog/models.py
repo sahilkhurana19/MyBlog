2b4541d46ff2aece697fe2ec202d36375bd093a1
sahilkhurana02 <sahil_khurana@outlook.com>
2017-03-30 22:39:19 +0530

    Model created

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,14 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length = 200)
+    desc_sh = models.TextField()
+    desc_lg = models.TextField()
+    create_date = models.DateTimeField(default = timezone.now())
+    published_date = models.DateTimeField(blank = True, null =  True)
+    read_time = models.IntegerField(default = 5)
