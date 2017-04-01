c2d871dafdc3db0e71ef4487b9a5ab192b776e8e
sahilkhurana02 <sahil_khurana@outlook.com>
2017-04-01 18:08:04 +0530

    Refined models and added admin

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,21 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.utils import timezone
+from django.utils.timezone import now
 
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length = 200)
-    desc_sh = models.TextField()
-    desc_lg = models.TextField()
-    create_date = models.DateTimeField(default = timezone.now())
-    published_date = models.DateTimeField(blank = True, null =  True)
+    desc_sh = models.TextField(verbose_name = 'Short Description')
+    desc_lg = models.TextField(verbose_name = 'Long Description')
+    create_date = models.DateTimeField(verbose_name = 'Created on:', default = now)
+    published_date = models.DateTimeField(verbose_name = 'Published on:', blank = True, null =  True)
     read_time = models.IntegerField(default = 5)
+
+    def publish():
+        self.published_date = timezone.now()
+        self.save()
+
+    def __unicode__(self):
+        return "%s" % (self.title)
