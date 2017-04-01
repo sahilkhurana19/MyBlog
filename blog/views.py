b8001644acbafcba57c59d140320350ecdb4c3d0
sahilkhurana02 <sahil_khurana@outlook.com>
2017-04-01 22:24:24 +0530

    Configured urls and index view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 
-def index():
+def index(request):
+    latest_posts = Post.objects.order_by('-published_date')[:5]
+    context = {'latest_posts':latest_posts}
+    return render(request, 'blog/index.html', context)
+
+def detail():
     pass
