420c635f8d94832dc504954c23a356a18b8c9af8
sahilkhurana02 <sahil_khurana@outlook.com>
2017-04-13 22:09:17 +0530

    Prep for deployment

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/1.10/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.10/ref/settings/
 """
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+
 
 import os
 
@@ -79,12 +82,17 @@ WSGI_APPLICATION = 'MyBlog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
@@ -205,3 +213,6 @@ CKEDITOR_CONFIGS = {
             ]),
     }
 }
+
+#Deployment
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
